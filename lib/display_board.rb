fefdf65924a264fbf8bbb075af83bde69b04501c
Josh Kramer <funemployeddad@gmail.com>
2017-11-16 00:13:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def rows
+  cell = "   "
+  vertical_divider = "|"
+  print "#{cell}#{vertical_divider}#{cell}#{vertical_divider}#{cell}"
+end
+
+def display_board
+  horizontal_divider = "-----------"
+   puts rows
+   puts horizontal_divider
+   puts rows
+   puts horizontal_divider
+   puts rows
+end
